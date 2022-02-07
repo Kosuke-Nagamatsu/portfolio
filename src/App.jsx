e152bc87cc21adfbe666b6bf35c2daf8fd866f3f
Kosuke-Nagamatsu <hateruma39@gmail.com>
2022-02-07 09:36:13 +0900

    update : add 'routes' variable to be managed as an array

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,27 @@ import { History } from './components/pages/history/History';
 import { Work } from './components/pages/work/Work';
 import { NotFound } from './components/pages/NotFound';
 
+const routes = [
+  { name: 'Home', path: '/', element: <Home /> },
+  { name: 'History', path: '/history', element: <History /> },
+  { name: 'Work', path: '/work', element: <Work /> },
+  { name: 'NotFound', path: '*', element: <NotFound /> }
+]
+
 export const App = () => {
   return (
+
     // basenameにPUBLIC_URLを設定
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Header />
+      {/* 配列の最後の要素を除いてHeaderへ渡す（NotFoundは使わないため） */}
+      <Header routes={routes.slice(0, -1)} />
       <MainContainer>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="*" element={<NotFound />} />
+          {
+            routes.map((route, index) => (
+              <Route path={route.path} element={route.element} key={index} />
+            ))
+          }
         </Routes>
       </MainContainer>
       <Footer />
