8acb1f75eff865f6e2829f092325d63b7c71d596
Kosuke-Nagamatsu <hateruma39@gmail.com>
2022-04-02 00:28:53 +0900

    fix: bugs on smartphone screens

--- src/hooks/createPetal.js
+++ src/hooks/createPetal.js
@@ -7,9 +7,17 @@ import classes from '../components/pages/home/CherryBlossom.module.scss';  // CS
 // この流れになっています。
 // Hooksは未使用ですが、可読性を上げるためhooksフォルダに関数を分離させました。
 // ほぼ参照させていただいたコード通りです。コードリーディングしながら微調整し、とても勉強になりました。
-// 
+
+// ■スマホ画面が小刻みに揺れる問題の対処メモ
+// 検証ツールでiPhone画面などのスマホ表示を見ると、画面が小刻みに揺れ出すことに気づく。
+// コンソールに window.innerWidth の値を出力し、ウィンドウの横幅を確かめてみる。
+// iPhoneSEの場合、通常 375px なのに数値が増えるときがあり、その瞬間から画面が揺れだす。
+// 花びらのサイズ分が、ウィンドウ幅からはみ出したときに起きる？と考える。
+// 花びらが降り始める位置の最大値を 花びらサイズ+10px 左にずらすことで値が一定し、解決した様子。
+
 
 const section = document.getElementsByClassName('cherry-blossom-container');
+const screenWidth = window.innerWidth;
 
 // 花びらを作る関数
 export const createPetal = () => {
@@ -18,11 +26,10 @@ export const createPetal = () => {
   petalEl.className = classes.petal;  // importしたscssファイルのpetalクラスを指定
   const minSize = 10;
   const maxSize = 15;
-  // Math.random() * (最大値 + 1 - 最小値) + 最小値  ←これで最大値〜最小値の乱数を出せるみたい
-  const size = Math.random() * (maxSize + 1 - minSize) + minSize;
+  const size = Math.random() * (maxSize + 1 - minSize) + minSize;  // Math.random() * （最大値 + 1 - 最小値） + 最小値  ←これで最小値〜最大値の乱数を出せるみたい
   petalEl.style.width = `${size}px`;
   petalEl.style.height = `${size}px`;
-  petalEl.style.left = `${Math.random() * window.innerWidth}px`;
+  petalEl.style.left = `${Math.random() * (screenWidth - size - 9)}px`;  // 上の乱数計算（最小値：0,  最大値：screenWidth - 花びらサイズ - 10）
 
   // HTMLCollectionのitemメソッドで花びらをsectionに追加
   section.item(0).appendChild(petalEl);
